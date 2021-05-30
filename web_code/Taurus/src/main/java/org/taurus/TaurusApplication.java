6b78983724fb32ed527de234a166259b16c788b6
lwx-xin <1314520lu>
2021-05-30 19:12:37 +0800

    init

--- web_code/Taurus/src/main/java/org/taurus/TaurusApplication.java
+++ web_code/Taurus/src/main/java/org/taurus/TaurusApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan(value = "org.taurus.dao")
+@EnableCaching
 public class TaurusApplication extends SpringBootServletInitializer {
 
 	@Override
--- web_code/Taurus/src/test/java/org/taurus/TaurusApplicationTests.java
+++ web_code/Taurus/src/test/java/org/taurus/TaurusApplicationTests.java
@@ -1,26 +1,12 @@
 package org.taurus;
 
-import javax.imageio.stream.FileImageOutputStream;
-
-import org.jaudiotagger.audio.AudioFileIO;
-import org.jaudiotagger.audio.AudioHeader;
-import org.jaudiotagger.audio.generic.GenericAudioHeader;
-import org.jaudiotagger.audio.mp3.MP3File;
-import org.jaudiotagger.audio.mp4.Mp4InfoReader;
-import org.jaudiotagger.tag.FieldKey;
-import org.jaudiotagger.tag.Tag;
-import org.jaudiotagger.tag.id3.AbstractID3v2Frame;
-import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
-import org.jaudiotagger.tag.id3.framebody.FrameBodyAPIC;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.taurus.common.LogManager;
 import org.taurus.config.load.data.InitAdminUserData;
 import org.taurus.config.load.properties.TaurusProperties;
-
-import java.io.File;
-import java.io.RandomAccessFile;
 
 @SpringBootTest
 class TaurusApplicationTests {
